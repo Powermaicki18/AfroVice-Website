839d70556e863a5f6df59b67b2eca856c8e0fc13
Miguel Nieto <migue418@hotmail.com>
2025-11-18 11:47:14 -0500

    Integrating API Events, on homepage and event page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Search, Bell, User } from 'lucide-react';
+import { User } from 'lucide-react';
 import logo from './logo.png';
 import {NavLink} from "react-router-dom";
 
@@ -49,19 +49,6 @@ export default function Navigation({ currentPage }: NavigationProps) {
           </div>
 
           <div className="flex items-center gap-4">
-            <div className="hidden sm:flex items-center gap-2 bg-white/10 rounded-full px-4 py-2">
-              <Search className="w-4 h-4 text-gray-400" />
-              <input
-                type="text"
-                placeholder="Buscar eventos..."
-                className="bg-transparent border-none outline-none text-white text-sm placeholder-gray-400 w-40"
-              />
-            </div>
-
-            <button className="text-gray-300 hover:text-white transition-colors">
-              <Bell className="w-5 h-5" />
-            </button>
-
             <button className="text-gray-300 hover:text-white transition-colors">
               <User className="w-8 h-8 rounded-full bg-violet-600 p-1" />
             </button>
--- src/lib/afrovice-api.ts
+++ src/lib/afrovice-api.ts
@@ -6,6 +6,60 @@ export type Artist = {
     photo: string;
 }
 
+export type NewArtist = Omit<Artist, 'id'>;
+
+export type Event = {
+    id: number;
+    name: string;
+    logo: string;
+    price: number;
+}
+
+export type NewEvent = Omit<Event, 'id'>;
+
+export type User = {
+    id: number;
+    name: string;
+    email: string;
+    photo: string;
+    role_id: number;
+}
+
+export type NewUser = Omit<User, 'id'>;
+
+export type Gender = {
+    id: number;
+    name: string;
+}
+
+export type NewGender= Omit<Gender, 'id'>;
+
+export type Presentation = {
+    id: number;
+    event_id: number;
+    flyer: string;
+    date_start: Date;
+    date_end: Date;
+}
+
+export type NewPresentation= Omit<Presentation, 'id'>;
+
+export type Ticket = {
+    id: number;
+    user_id: number;
+    presentation_id: number;
+}
+
+export type NewTicket= Omit<Ticket, 'id'>;
+
+export type Comment = {
+    id: number;
+    message: string;
+    presentation_id: number;
+    user_id: number;
+}
+
+export type NewComment= Omit<Comment, 'id'>;
 
 class AfroviceApi {
     private http: AxiosInstance;
@@ -18,6 +72,108 @@ class AfroviceApi {
 
     async getArtists(): Promise<Array<Artist>> {
         const response = await this.http.get<Array<Artist>>('/artists');
+
+        return response.data;
+    }
+
+    async createArtist(artist: NewArtist): Promise<Artist> {
+        const response = await this.http.post<Artist>('/artists', artist);
+
+        return response.data;
+    }
+
+    async getArtist(artistId: Artist['id']): Promise<Artist> {
+        const response = await this.http.get<Artist>(`/artists/${artistId}`);
+
+        return response.data;
+    }
+
+    async getEvents(): Promise<Array<Event>> {
+        const response = await this.http.get<Array<Event>>('/events');
+
+        return response.data;
+    }
+
+    async createEvent(event: NewEvent): Promise<Event> {
+        const response = await this.http.post<Event>('/events', event);
+
+        return response.data;
+    }
+
+    async getEvent(eventId: Event['id']): Promise<Event> {
+        const response = await this.http.get<Event>(`/events/${eventId}`);
+
+        return response.data;
+    }
+
+    async getUsers(): Promise<Array<User>> {
+        const response = await this.http.get<Array<User>>('/users');
+
+        return response.data;
+    }
+
+    async getUser(userId: User['id']): Promise<User> {
+        const response = await this.http.get<User>(`/users/${userId}`);
+
+        return response.data;
+    }
+
+    async createUser(user: NewUser): Promise<User> {
+        const response = await this.http.post<User>('/users', user);
+
+        return response.data;
+    }
+
+    async getGenders(): Promise<Array<Gender>> {
+        const response = await this.http.get<Array<Gender>>('/genders');
+
+        return response.data;
+    }
+
+    async createGender(gender: NewGender): Promise<Gender> {
+        const response = await this.http.post<User>('/genders', gender);
+
+        return response.data;
+    }
+
+    async getPresentations(): Promise<Array<Presentation>> {
+        const response = await this.http.get<Array<Presentation>>('/presentations');
+
+        return response.data;
+    }
+
+    async createPresentation(presentation: NewPresentation): Promise<Presentation> {
+        const response = await this.http.post<Presentation>('/presentations', presentation);
+
+        return response.data;
+    }
+
+    async getPresentation(presentationId: Presentation['id']): Promise<Presentation> {
+        const response = await this.http.get<Presentation>(`/presentations/${presentationId}`);
+
+        return response.data;
+    }
+
+    async getTickets(): Promise<Array<Ticket>> {
+        const response = await this.http.get<Array<Ticket>>('/tickets');
+
+        return response.data;
+    }
+
+    async creatTicket(ticket: NewTicket): Promise<Ticket> {
+        const response = await this.http.post<Ticket>('/tickets', ticket);
+
+        return response.data;
+    }
+
+    async getComments(): Promise<Array<Comment>> {
+        const response = await this.http.get<Array<Comment>>('/comments');
+
+        return response.data;
+    }
+
+    async createComment(comment: NewComment): Promise<Comment> {
+        const response = await this.http.post<Comment>('/comments', comment);
 
         return response.data;
     }
